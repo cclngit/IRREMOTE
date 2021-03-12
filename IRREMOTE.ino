496a0a23488d5868d4f7676e9e1b8a7a7f106dc7
cclngit <38783129+cclngit@users.noreply.github.com>
2021-03-12 23:06:43 +0100

    Initial commit

---
+++ IRREMOTE.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       IRREMOTE.ino
+    Created:	23/01/2021 15:20:53
+    Author:     CCLN\cicil
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+    Serial.begin(9200);
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
